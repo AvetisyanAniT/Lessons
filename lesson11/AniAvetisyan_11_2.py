405023f69ce50014e2520ac02a7b2aaf70f63a7f
Ani <73894055+AvetisyanAniT@users.noreply.github.com>
2022-01-30 18:08:57 +0400

    Add files via upload

---
+++ lesson11/AniAvetisyan_11_2.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+
+try:
+    tree = ET.parse('homework.xml')
+    root=tree.getroot()
+    for item in root.findall('subject'):
+        if item.find('name').text=='Math':
+            print(item.find('totalpoint').text)
+    # tree = ET.parse('homework.xml')
+    # root = tree.getroot()
+    # print()
+    # for item in root.findall('subject'):
+    #     if item.find('name').text == 'Math':
+    #         print('Total point from Math is', item.find('totalpoint').text)
+except:
+    print('Something went wrong')
+finally:
+    print('XML')
+
+
+# import json
+
+# try:
+#     with open('jsonhome.json') as f:
+#         data = json.load(f)
+#         exams = data['Exams']
+#         subject = exams['Subject']
+
+#         for item in subject:
+#             if item['name'] == 'Math':
+#                 print('Total point from Math is', item['totalpoint'])
+# except:
+#     print('Something went wrong')
+# finally:
+#     print('JSON')
