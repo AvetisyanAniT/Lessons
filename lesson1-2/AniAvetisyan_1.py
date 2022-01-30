04f147342ef5b2d98f8996d3de7babefabcc3c79
Ani <73894055+AvetisyanAniT@users.noreply.github.com>
2022-01-30 17:33:57 +0400

    Add files via upload

---
+++ lesson1-2/AniAvetisyan_1.py
@@ -0,0 +1,4 @@
+print("Welcome to python")
+print(2*"\n")
+print("word1", end=" ! " ); print("word2")
+print("Ani", end="@")
---
+++ lesson1-2/AniAvetisyan_2.py
@@ -0,0 +1,40 @@
+# a = 1
+# print(type(a))
+# print(id(a))
+# print(a)
+
+# a = 1
+# b = [1,2,3]
+# print(type(a))
+# # print(a)
+# print(type(b[0]))
+# # print(b)
+
+# a = [1, 2, 3, 'test']
+# print(id(a))
+# print(type(a))
+# # print(a)
+# print(id(a[0]))
+# print(type(a[0]))
+# # print(a[0])
+# print(id(a[1]))
+# print(type(a[1]))
+# # print(a[1])
+# print(id(a[2]))
+# print(type(a[2]))
+# # print(a[2])
+# print(id(a[3]))
+# print(type(a[3]))
+# # print(a[3])
+
+# a = 2 correct
+# $b=1 not correct
+# !c_d=2 not correct
+# _at = 1 correct
+# else=10 not correct
+# True=20 not correct
+# []=10 not correct
+
+# a, *b = 100,'test', 10
+# print(a)
+# print(b)
